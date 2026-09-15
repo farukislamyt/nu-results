bbdf2b1ee5bd2d8f3383f3894d00658532519b24
K M Faruk islam <49297348+farukislamyt@users.noreply.github.com>
2026-09-15 17:00:28 +0600

    Update Masters compatibility exports

--- masters.py
+++ masters.py
@@ -1,3 +1,3 @@
-from api.masters import MASTERS_EXAMINATIONS, MASTERS_URL, parse_masters_result
+from api.masters import MASTERS_CAPTCHA_REFRESH_URL, MASTERS_EXAMINATIONS, MASTERS_URL, parse_masters_result
 
-__all__ = ["MASTERS_EXAMINATIONS", "MASTERS_URL", "parse_masters_result"]
+__all__ = ["MASTERS_CAPTCHA_REFRESH_URL", "MASTERS_EXAMINATIONS", "MASTERS_URL", "parse_masters_result"]
