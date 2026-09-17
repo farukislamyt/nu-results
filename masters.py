b3a133decd21214c320ce82a237aefd18f2a46f9
K M Faruk islam <49297348+farukislamyt@users.noreply.github.com>
2026-09-17 17:36:32 +0600

    refactor: remove obsolete masters shim

--- masters.py
+++
@@ -1,3 +0,0 @@
-from api.masters import MASTERS_CAPTCHA_REFRESH_URL, MASTERS_EXAMINATIONS, MASTERS_URL, parse_masters_result
-
-__all__ = ["MASTERS_CAPTCHA_REFRESH_URL", "MASTERS_EXAMINATIONS", "MASTERS_URL", "parse_masters_result"]
